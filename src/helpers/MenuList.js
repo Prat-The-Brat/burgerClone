d4176fcae3d150d771969ca709cf8fdcb8168da9
Prat <99562855+Prat-The-Brat@users.noreply.github.com>
2023-06-05 12:03:25 +0530

    inital commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import cheeseburger from "../assets/cheeseburger.jpg";
+import doubleBurger from "../assets/doubleBurger.jpg";
+import friesCombo from "../assets/friesCombo.jpg";
+import singleBurger from "../assets/singleBurger.jpg";
+import waffleburger from "../assets/waffleburger.jpg";
+import tripleBurger from "../assets/tripleBurger.jpg";
+
+export const MenuList = [
+  {
+    name: "Cheese Burger",
+    image: cheeseburger,
+    price: 15.99,
+  },
+  {
+    name: "Double Burger",
+    image: doubleBurger,
+    price: 11.99,
+  },
+  {
+    name: "Fries combo Burger",
+    image: friesCombo,
+    price: 256.53,
+  },
+  {
+    name: "Single Burger",
+    image: singleBurger,
+    price: 17.99,
+  },
+  {
+    name: "Waffle Burger",
+    image: waffleburger,
+    price: 4.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: tripleBurger,
+    price: 1997.99,
+  },
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/burger.jpeg";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1> prat's burguria </h1>
+        <p> BURGER TO FIT ANY TASTE</p>
+        <Link to="/menu">
+          <button> ORDER NOW </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
